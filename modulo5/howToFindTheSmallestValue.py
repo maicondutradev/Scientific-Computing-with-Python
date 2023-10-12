90dae73dbfae791c8b7e2c909c4512cc170852fa
maicondutradev <109883278+maicondutradev@users.noreply.github.com>
2023-10-12 09:51:01 -0300

    Fim do módulo 5

---
+++ modulo5/howToFindTheSmallestValue.py
@@ -0,0 +1,7 @@
+largestSoFar = -1
+print('Before', largestSoFar)
+for theNum in [9, 41, 12, 3, 74, 15]:
+    if theNum > largestSoFar:
+        largestSoFar = theNum
+    print(largestSoFar, theNum)
+print('After', largestSoFar)
